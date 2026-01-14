404fd9b22481c6ffcdb666b1b81f9305c624552e
Zihang <zengzihang54@gmail.com>
2026-01-14 02:56:08 +0000

    update train.sh

--- train.sh
+++ train.sh
@@ -14,23 +14,23 @@ pip install torch torchvision
 pip install sconf>=0.2.3 lmdb>=1.2.1
 pip install tqdm pillow opencv-python
 
+cd /code/FsFont
+
 # # === Step1: 解压数据集 ===
 # tar -xzf /share/home/tm945458209690000/a945500620/Zzh-FsFont/font.tar.gz -C /share/home/tm945458209690000/a945500620/Zzh-FsFont
 
-# === Step2: 构建数据库 ===
-ROOT_DIR=/share/home/tm945458209690000/a945500620/Zzh-FsFont
+# # === Step2: 构建数据库 ===
+# ROOT_DIR=/share/home/tm945458209690000/a945500620/Zzh-FsFont
 
-echo "Building dataset..."
+# echo "Building dataset..."
 
-cd /code/FsFont
-
-python3 ./build_dataset/build_meta4train.py \
-  --saving_dir $ROOT_DIR/results/result \
-  --content_font $ROOT_DIR/songti/imgs \
-  --train_font_dir $ROOT_DIR/png_style_format5/Pot1.0Train \
-  --val_font_dir $ROOT_DIR/png_style_format5/Pot1.0Test \
-  --seen_unis_file ./meta/train_unis.json \
-  --unseen_unis_file ./meta/val_unis.json
+# python3 ./build_dataset/build_meta4train.py \
+#   --saving_dir $ROOT_DIR/results/result \
+#   --content_font $ROOT_DIR/songti/imgs \
+#   --train_font_dir $ROOT_DIR/png_style_format5/Pot1.0Train \
+#   --val_font_dir $ROOT_DIR/png_style_format5/Pot1.0Test \
+#   --seen_unis_file ./meta/train_unis.json \
+#   --unseen_unis_file ./meta/val_unis.json
 
 # === Step2: 启动训练 ===
 echo "Start training..."
